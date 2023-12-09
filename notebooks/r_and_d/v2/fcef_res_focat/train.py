d400cc2a29e47ea38f1aba848f6d695cd88381f1
ashen007 <asheniranga@gmail.com>
2023-12-09 22:00:41 +0530

    train fcef res focus attention

---
+++ notebooks/r_and_d/v2/fcef_res_focat/train.py
@@ -0,0 +1,21 @@
+from torch import nn
+
+from notebooks.r_and_d.net_trainer import *
+from forest_cover_change_detection.models.v2.single_in.fc_ef_res_focat import FCFEResWithFocAt
+
+if __name__ == "__main__":
+    config = Config('../../../../data/annotated/',
+                    '../../../../data/annotated/train.csv',
+                    '../../../../data/annotated/test.csv',
+                    FCFEResWithFocAt(6, 2),
+                    nn.NLLLoss,
+                    300,
+                    32,
+                    restore_best=True,
+                    concat=True,
+                    patched=False)
+
+    df = pd.read_csv(config.test)
+
+    do(config)
+    evaluate(df, config)
