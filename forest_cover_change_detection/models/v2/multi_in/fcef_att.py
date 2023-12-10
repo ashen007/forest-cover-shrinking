2978d90009f9430753b0756a3832dd505b698f83
ashen007 <asheniranga@gmail.com>
2023-12-10 20:24:25 +0530

    train multi input fcef resnst architecture

--- forest_cover_change_detection/models/v2/multi_in/fcef_att.py
+++ forest_cover_change_detection/models/v2/multi_in/fcef_att.py
@@ -2,84 +2,73 @@ import torch
 
 from torch import nn
 from torch.nn.modules.padding import ReplicationPad2d
-from forest_cover_change_detection.models.fcef.modules import ResidualDownSample, UpSample
-from forest_cover_change_detection.models.fcef.modules import ResNeStBlock
+from forest_cover_change_detection.models.fcef.modules import UpSample, DownSample
+from forest_cover_change_detection.models.fcfe_with_att.modules import AdditiveAttentionGate
 
 
-class FCFEResSplitAttention(nn.Module):
+class FCEFVanila(nn.Module):
+    """
+    base architecture, testing different tactics to improve
+    feature selection and extractions while decoder is fixed
+    """
 
     def __init__(self, in_channels, classes, kernel=3):
-        super(FCFEResSplitAttention, self).__init__()
+        super(FCEFVanila, self).__init__()
         filters = [16, 32, 64, 128, 256]
-        self.drop = nn.Dropout(0.2)
 
         # down sampling
-        self.feat_ext_block_1 = nn.Sequential(ResidualDownSample(in_channels, filters[0]),
-                                              ResNeStBlock(filters[0], filters[0], 1, 3),
-                                              ResidualDownSample(filters[0], filters[0]),
-                                              ResNeStBlock(filters[0], filters[0], 1, 3))
+        # this block are the changing sections, can try different feature extractors
+        self.feat_ext_block_1 = DownSample(in_channels, filters[0], dropout=False, blocks=2)
         self.dwn_block_1 = nn.MaxPool2d(2)  # (16, 128, 128)
+        # attention gate will use output of the above down-sampled layer
 
-        self.feat_ext_block_2 = nn.Sequential(ResidualDownSample(filters[0], filters[1]),
-                                              ResNeStBlock(filters[1], filters[1], 1, 3),
-                                              ResidualDownSample(filters[1], filters[1]),
-                                              ResNeStBlock(filters[1], filters[1], 1, 3))
+        # these blocks are the changing sections, can try different feature extractors
+        self.feat_ext_block_2 = DownSample(filters[0], filters[1], dropout=False, blocks=2)
         self.dwn_block_2 = nn.MaxPool2d(2)  # (32, 64, 64)
+        # attention gate will use output of the above down-sampled layer
 
-        self.feat_ext_block_3 = nn.Sequential(ResidualDownSample(filters[1], filters[2]),
-                                              ResNeStBlock(filters[2], filters[2], 1, 3),
-                                              ResidualDownSample(filters[2], filters[2]),
-                                              ResNeStBlock(filters[2], filters[2], 1, 3))
+        # these blocks are the changing sections, can try different feature extractors
+        self.feat_ext_block_3 = DownSample(filters[1], filters[2], dropout=False, blocks=2)
         self.dwn_block_3 = nn.MaxPool2d(2)  # (64, 32, 32)
+        # attention gate will use output of the above down-sampled layer
 
-        self.feat_ext_block_4 = nn.Sequential(ResidualDownSample(filters[2], filters[3]),
-                                              ResNeStBlock(filters[3], filters[3], 1, 3),
-                                              ResidualDownSample(filters[3], filters[3]),
-                                              ResNeStBlock(filters[3], filters[3], 1, 3))
+        # these blocks are the changing sections, can try different feature extractors
+        self.feat_ext_block_4 = DownSample(filters[2], filters[3], dropout=False, blocks=2)
         self.dwn_block_4 = nn.MaxPool2d(2)  # (128, 16, 16)
+        # attention gate will use output of the above down-sampled layer
 
-        self.feat_ext_block_5 = nn.Sequential(ResidualDownSample(filters[3], filters[4]),
-                                              ResNeStBlock(filters[4], filters[4], 1, 3),
-                                              ResidualDownSample(filters[4], filters[4]),
-                                              ResNeStBlock(filters[4], filters[4], 1, 3))
+        # these blocks are the changing sections, can try different feature extractors
+        self.feat_ext_block_5 = DownSample(filters[3], filters[4], dropout=False, blocks=2)
         self.dwn_block_5 = nn.MaxPool2d(2)  # (256, 8, 8)
+        # attention gate will use output of the above down-sampled layer
 
         # up sampling
-        self.up_feat_ext_block_1 = nn.Sequential(ResidualDownSample(filters[4], filters[4], kernel),
-                                                 ResNeStBlock(filters[4], filters[4], 1, 3),
-                                                 ResidualDownSample(filters[4], filters[4]))
+        # this is a common up-sample block for all models
+        self.up_feat_ext_block_1 = UpSample(filters[4], filters[4], kernel, padding=1, blocks=2)
         # this block is the layer that increases the dimensions by factor 2
         self.up_block_1 = UpSample(filters[4], filters[4], kernel,
                                    stride=2, padding=0, output_padding=1, blocks=1)  # (256, 16, 16)
 
         # this is a common up-sample block for all models
-        self.up_feat_ext_block_2 = nn.Sequential(ResidualDownSample(3 * filters[3], filters[4], kernel),
-                                                 ResNeStBlock(filters[4], filters[4], 1, 3),
-                                                 ResidualDownSample(filters[4], filters[4]))
+        self.up_feat_ext_block_2 = UpSample(3 * filters[3], filters[4], kernel, padding=1, blocks=2)
         # this block is the layer that increases the dimensions by factor 2
         self.up_block_2 = UpSample(filters[4], filters[3], kernel,
                                    stride=2, padding=0, output_padding=1, blocks=1)  # (128, 32, 32)
 
         # this is a common up-sample block for all models
-        self.up_feat_ext_block_3 = nn.Sequential(ResidualDownSample(3 * filters[2], filters[3], kernel),
-                                                 ResNeStBlock(filters[3], filters[3], 1, 3),
-                                                 ResidualDownSample(filters[3], filters[3]))
+        self.up_feat_ext_block_3 = UpSample(3 * filters[2], filters[3], kernel, padding=1, blocks=2)
         # this block is the layer that increases the dimensions by factor 2
         self.up_block_3 = UpSample(filters[3], filters[2], kernel,
                                    stride=2, padding=0, output_padding=1, blocks=1)  # (64, 64, 64)
 
         # this is a common up-sample block for all models
-        self.up_feat_ext_block_4 = nn.Sequential(ResidualDownSample(3 * filters[1], filters[2], kernel),
-                                                 ResNeStBlock(filters[2], filters[2], 1, 3),
-                                                 ResidualDownSample(filters[2], filters[2]))
+        self.up_feat_ext_block_4 = UpSample(3 * filters[1], filters[2], kernel, padding=1, blocks=2)
         # this block is the layer that increases the dimensions by factor 2
         self.up_block_4 = UpSample(filters[2], filters[1], kernel,
                                    stride=2, padding=0, output_padding=1, blocks=1)  # (32, 128, 128)
 
         # this is a common up-sample block for all models
-        self.up_feat_ext_block_5 = nn.Sequential(ResidualDownSample(3 * filters[0], filters[1], kernel),
-                                                 ResNeStBlock(filters[1], filters[1], 1, 3),
-                                                 ResidualDownSample(filters[1], filters[1]))
+        self.up_feat_ext_block_5 = UpSample(3 * filters[0], filters[1], kernel, padding=1, blocks=2)
         # this block is the layer that increases the dimensions by factor 2
         self.up_block_5 = UpSample(filters[1], filters[0], kernel,
                                    stride=2, padding=0, output_padding=1, blocks=1)  # (16, 256, 256)
@@ -87,7 +76,7 @@ class FCFEResSplitAttention(nn.Module):
         self.out_block = nn.Sequential(nn.Conv2d(filters[0], classes, kernel, padding=1),
                                        nn.LogSoftmax(dim=1))  # (2, 256, 256)
 
-    def forward(self, x_1, x_2):
+    def forward(self, x_1, x_2):  # (16, 6, 128, 128)
         s_0 = x_1.shape
 
         # branch 1
@@ -131,30 +120,34 @@ class FCFEResSplitAttention(nn.Module):
         xu_1_b = self.up_feat_ext_block_1((xd_5_b1 - xd_5_b2))
         xu_1 = self.up_block_1(xu_1_b)
         pad_1 = ReplicationPad2d((0, (s_4[3] - xu_1.shape[3]), 0, (s_4[2] - xu_1.shape[2])))
+        at_gate_1 = AdditiveAttentionGate(xu_1_b.shape[1], s_4[1])
 
         # concat skip connection and up-sampled layer from below
-        xu_1 = torch.cat(((xd_4_b1 - xd_4_b2), pad_1(xu_1)), dim=1)
+        xu_1 = torch.cat((at_gate_1((xd_4_b1 - xd_4_b2), xu_1_b), pad_1(xu_1)), dim=1)
 
         xu_2_b = self.up_feat_ext_block_2(xu_1)
         xu_2 = self.up_block_2(xu_2_b)
         pad_2 = ReplicationPad2d((0, (s_3[3] - xu_2.shape[3]), 0, (s_3[2] - xu_2.shape[2])))
+        at_gate_2 = AdditiveAttentionGate(xu_2_b.shape[1], s_3[1])
 
         # concat skip connection and up-sampled layer from below
-        xu_2 = torch.cat(((xd_3_b1 - xd_3_b2), pad_2(xu_2)), dim=1)
+        xu_2 = torch.cat((at_gate_2((xd_3_b1 - xd_3_b2), xu_2_b), pad_2(xu_2)), dim=1)
 
         xu_3_b = self.up_feat_ext_block_3(xu_2)
         xu_3 = self.up_block_3(xu_3_b)
         pad_3 = ReplicationPad2d((0, (s_2[3] - xu_3.shape[3]), 0, (s_2[2] - xu_3.shape[2])))
+        at_gate_3 = AdditiveAttentionGate(xu_3_b.shape[1], s_2[1])
 
         # concat skip connection and up-sampled layer from below
-        xu_3 = torch.cat(((xd_2_b1 - xd_2_b2), pad_3(xu_3)), dim=1)
+        xu_3 = torch.cat((at_gate_3((xd_2_b1 - xd_2_b2), xu_3_b), pad_3(xu_3)), dim=1)
 
         xu_4_b = self.up_feat_ext_block_4(xu_3)
         xu_4 = self.up_block_4(xu_4_b)
         pad_4 = ReplicationPad2d((0, (s_1[3] - xu_4.shape[3]), 0, (s_1[2] - xu_4.shape[2])))
+        at_gate_4 = AdditiveAttentionGate(xu_4_b.shape[1], s_1[1])
 
         # concat skip connection and up-sampled layer from below
-        xu_4 = torch.cat(((xd_1_b1 - xd_1_b2), pad_4(xu_4)), dim=1)
+        xu_4 = torch.cat((at_gate_4((xd_1_b1 - xd_1_b2), xu_4_b), pad_4(xu_4)), dim=1)
 
         xu_5_b = self.up_feat_ext_block_5(xu_4)
         xu_5 = self.up_block_5(xu_5_b)
@@ -168,7 +161,7 @@ class FCFEResSplitAttention(nn.Module):
 if __name__ == '__main__':
     t_1 = torch.randn(16, 3, 128, 128).cuda()
     t_2 = torch.randn(16, 3, 128, 128).cuda()
-    model = FCFEResSplitAttention(3, 2)
+    model = FCEFVanila(3, 2)
     model.cuda()
 
     print(model(t_1, t_2).shape)
---
+++ notebooks/r_and_d/metrics.py
@@ -0,0 +1,78 @@
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def metric_summary(root, renames):
+    comp_df = []
+
+    for sub in os.listdir(root):
+        sub_dir = os.path.join(root, sub)
+
+        if os.path.isdir(sub_dir):
+            if sub.startswith('fc'):
+                metric_file = os.path.join(sub_dir, 'metric_eval.csv')
+                sub_metrics = pd.read_csv(metric_file)
+                comp_df.append(sub_metrics.mean())
+
+    comp_df = pd.concat(comp_df, axis=1)
+    comp_df = comp_df.rename(columns=renames).T
+    comp_df['f1'] = comp_df.apply(lambda x: 2 * ((x['precision'] * x['recall']) / (x['precision'] + x['recall'])),
+                                  axis=1)
+
+    return comp_df
+
+
+def training_summary(root):
+    train_loss_df = pd.DataFrame()
+    val_loss_df = pd.DataFrame()
+
+    for sub in os.listdir(root):
+        sub_dir = os.path.join(root, sub)
+
+        if os.path.isdir(sub_dir):
+            if sub.startswith('fc'):
+                metric_file = os.path.join(sub_dir, 'results.csv')
+                sub_metrics = pd.read_csv(metric_file).rename(
+                    columns={'train loss': f"t_{sub}",
+                             'val loss': f'v_{sub}'})
+
+                train_loss_df = pd.concat((train_loss_df,
+                                           sub_metrics[f't_{sub}'])
+                                          , axis=1)
+                val_loss_df = pd.concat((val_loss_df,
+                                         sub_metrics[f'v_{sub}'])
+                                        , axis=1)
+
+    return train_loss_df, val_loss_df
+
+
+def training_curves(t, v, labels):
+    plt.figure(figsize=(24, 6), dpi=300)
+    plt.subplot(1, 2, 1)
+    sns.lineplot(data=t, dashes=False,
+                 palette=sns.color_palette('BrBG', 9), ci=None)
+    plt.xlabel('epochs')
+    plt.ylabel('train loss')
+    plt.legend(labels)
+
+    plt.subplot(1, 2, 2)
+    sns.lineplot(data=v, dashes=False,
+                 palette=sns.color_palette('BrBG', 9), ci=None)
+    plt.xlabel('epochs')
+    plt.ylabel('val loss')
+    plt.legend(labels)
+    plt.show()
+
+
+def model_heatmap(df, labels):
+    plt.figure(figsize=(12, 6), dpi=200)
+    g = sns.heatmap(df,
+                    annot=True,
+                    cbar=False,
+                    cmap=sns.color_palette('BrBG'))
+    g.set_yticklabels(labels)
+    g.set_xticklabels(['overall acc', 'chng acc', 'no chng acc',
+                       'precision', 'recall', 'dice', 'kappa', 'f-1'])
+    plt.show()
