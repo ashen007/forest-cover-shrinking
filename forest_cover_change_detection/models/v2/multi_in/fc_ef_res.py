7184d151dc5715de5ba624679d46adcec5d02ded
ashen007 <asheniranga@gmail.com>
2023-12-10 11:52:36 +0530

    train multi input fcef res architecture

--- forest_cover_change_detection/models/v2/multi_in/fc_ef_res.py
+++ forest_cover_change_detection/models/v2/multi_in/fc_ef_res.py
@@ -71,56 +71,74 @@ class FCFERes(nn.Module):
         self.out_block = nn.Sequential(nn.Conv2d(filters[0], classes, kernel, padding=1),
                                        nn.LogSoftmax(dim=1))  # (2, 256, 256)
 
-    def forward(self, x):
-        s_0 = x.shape
+    def forward(self, x_1, x_2):
+        s_0 = x_1.shape
 
-        xd_1_fet_ext = self.feat_ext_block_1(x)
-        xd_1 = self.dwn_block_1(xd_1_fet_ext)  # 1(6, 16, 48, 48)
-        s_1 = xd_1.shape
+        # branch 1
+        xd_1_fet_ext_b1 = self.feat_ext_block_1(x_1)
+        xd_1_b1 = self.dwn_block_1(xd_1_fet_ext_b1)  # 1(6, 16, 64, 64)
+        s_1 = xd_1_b1.shape
 
-        xd_2_fet_ext = self.feat_ext_block_2(xd_1)
-        xd_2 = self.dwn_block_2(xd_2_fet_ext)  # 1(6, 32, 24, 24)
-        s_2 = xd_2.shape
+        xd_2_fet_ext_b1 = self.feat_ext_block_2(xd_1_b1)
+        xd_2_b1 = self.dwn_block_2(xd_2_fet_ext_b1)  # 1(6, 32, 32, 32)
+        s_2 = xd_2_b1.shape
 
-        xd_3_fet_ext = self.feat_ext_block_3(xd_2)
-        xd_3 = self.dwn_block_3(xd_3_fet_ext)  # 1(6, 64, 12, 12)
-        s_3 = xd_3.shape
+        xd_3_fet_ext_b1 = self.feat_ext_block_3(xd_2_b1)
+        xd_3_b1 = self.dwn_block_3(xd_3_fet_ext_b1)  # 1(6, 64, 16, 16)
+        s_3 = xd_3_b1.shape
 
-        xd_4_fet_ext = self.feat_ext_block_4(xd_3)
-        xd_4 = self.dwn_block_4(xd_4_fet_ext)  # (16, 128, 6, 6)
-        s_4 = xd_4.shape
+        xd_4_fet_ext_b1 = self.feat_ext_block_4(xd_3_b1)
+        xd_4_b1 = self.dwn_block_4(xd_4_fet_ext_b1)  # (16, 128, 8, 8)
+        s_4 = xd_4_b1.shape
 
-        xd_5_fet_ext = self.feat_ext_block_5(xd_4)
-        xd_5 = self.dwn_block_5(xd_5_fet_ext)  # (16, 256, 3, 3)
-        s_5 = xd_5.shape
+        xd_5_fet_ext_b1 = self.feat_ext_block_5(xd_4_b1)
+        xd_5_b1 = self.dwn_block_5(xd_5_fet_ext_b1)  # (16, 256, 4, 4)
+        s_5 = xd_5_b1.shape
 
-        xu_1_b = self.up_feat_ext_block_1(xd_5)
+        # branch 2
+        xd_1_fet_ext_b2 = self.feat_ext_block_1(x_2)
+        xd_1_b2 = self.dwn_block_1(xd_1_fet_ext_b2)  # 1(6, 16, 64, 64)
+
+        xd_2_fet_ext_b2 = self.feat_ext_block_2(xd_1_b2)
+        xd_2_b2 = self.dwn_block_2(xd_2_fet_ext_b2)  # 1(6, 32, 32, 32)
+
+        xd_3_fet_ext_b2 = self.feat_ext_block_3(xd_2_b2)
+        xd_3_b2 = self.dwn_block_3(xd_3_fet_ext_b2)  # 1(6, 64, 16, 16)
+
+        xd_4_fet_ext_b2 = self.feat_ext_block_4(xd_3_b2)
+        xd_4_b2 = self.dwn_block_4(xd_4_fet_ext_b2)  # (16, 128, 8, 8)
+
+        xd_5_fet_ext_b2 = self.feat_ext_block_5(xd_4_b2)
+        xd_5_b2 = self.dwn_block_5(xd_5_fet_ext_b2)  # (16, 256, 4, 4)
+
+        # decoder
+        xu_1_b = self.up_feat_ext_block_1((xd_5_b1 - xd_5_b2))
         xu_1 = self.up_block_1(xu_1_b)
         pad_1 = ReplicationPad2d((0, (s_4[3] - xu_1.shape[3]), 0, (s_4[2] - xu_1.shape[2])))
 
         # concat skip connection and up-sampled layer from below
-        xu_1 = torch.cat((xd_4, pad_1(xu_1)), dim=1)
+        xu_1 = torch.cat(((xd_4_b1 - xd_4_b2), pad_1(xu_1)), dim=1)
 
         xu_2_b = self.up_feat_ext_block_2(xu_1)
         xu_2 = self.up_block_2(xu_2_b)
         pad_2 = ReplicationPad2d((0, (s_3[3] - xu_2.shape[3]), 0, (s_3[2] - xu_2.shape[2])))
 
         # concat skip connection and up-sampled layer from below
-        xu_2 = torch.cat((xd_3, pad_2(xu_2)), dim=1)
+        xu_2 = torch.cat(((xd_3_b1 - xd_3_b2), pad_2(xu_2)), dim=1)
 
         xu_3_b = self.up_feat_ext_block_3(xu_2)
         xu_3 = self.up_block_3(xu_3_b)
         pad_3 = ReplicationPad2d((0, (s_2[3] - xu_3.shape[3]), 0, (s_2[2] - xu_3.shape[2])))
 
         # concat skip connection and up-sampled layer from below
-        xu_3 = torch.cat((xd_2, pad_3(xu_3)), dim=1)
+        xu_3 = torch.cat(((xd_2_b1 - xd_2_b2), pad_3(xu_3)), dim=1)
 
         xu_4_b = self.up_feat_ext_block_4(xu_3)
         xu_4 = self.up_block_4(xu_4_b)
         pad_4 = ReplicationPad2d((0, (s_1[3] - xu_4.shape[3]), 0, (s_1[2] - xu_4.shape[2])))
 
         # concat skip connection and up-sampled layer from below
-        xu_4 = torch.cat((xd_1, pad_4(xu_4)), dim=1)
+        xu_4 = torch.cat(((xd_1_b1 - xd_1_b2), pad_4(xu_4)), dim=1)
 
         xu_5_b = self.up_feat_ext_block_5(xu_4)
         xu_5 = self.up_block_5(xu_5_b)
@@ -132,7 +150,8 @@ class FCFERes(nn.Module):
 
 
 if __name__ == '__main__':
-    t = torch.randn(16, 6, 256, 256)
+    t_1 = torch.randn(16, 6, 128, 128)
+    t_2 = torch.randn(16, 6, 128, 128)
     model = FCFERes(6, 2)
 
-    print(model(t).shape)
+    print(model(t_1, t_2).shape)
