0b85b667ec5180dee660cf9ab4c34b459bc93f7b
ashen007 <asheniranga@gmail.com>
2023-12-23 22:35:24 +0530

    train combine gate structure (focat) resnest

--- runs/r_and_d/v2/comb_chsp_at/fcef_resnst_focat/train.py
+++ runs/r_and_d/v2/comb_chsp_at/fcef_resnst_focat/train.py
@@ -4,9 +4,9 @@ from runs.r_and_d.net_trainer import *
 from forest_cover_change_detection.models.v2.single_in.combine_attention.fc_ef_resnst_focat import FCFEResNstWithFocAt
 
 if __name__ == "__main__":
-    config = Config('../../../../data/annotated/',
-                    '../../../../data/annotated/train.csv',
-                    '../../../../data/annotated/test.csv',
+    config = Config('../../../../../data/annotated/',
+                    '../../../../../data/annotated/train.csv',
+                    '../../../../../data/annotated/test.csv',
                     FCFEResNstWithFocAt(6, 2),
                     nn.NLLLoss,
                     300,
