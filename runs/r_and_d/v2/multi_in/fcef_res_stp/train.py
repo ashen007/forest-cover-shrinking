8fe21d4ffea69a26bba0e8d5446f230fa7a6a13f
ashen007 <asheniranga@gmail.com>
2023-12-25 14:33:54 +0530

    train a combination attention (strip pooling) gate with multi-in architectures

---
+++ runs/r_and_d/v2/multi_in/fcef_res_stp/train.py
@@ -0,0 +1,21 @@
+from torch import nn
+from runs.r_and_d.net_trainer import *
+from forest_cover_change_detection.models.v2.multi_in.combine_attention.fc_ef_res_stp import FCFEResSTP
+
+if __name__ == "__main__":
+    config = Config('../../../../../data/annotated/',
+                    '../../../../../data/annotated/train.csv',
+                    '../../../../../data/annotated/test.csv',
+                    FCFEResSTP(3, 2),
+                    nn.NLLLoss,
+                    300,
+                    32,
+                    restore_best=True,
+                    concat=False,
+                    multi_in=True,
+                    patched=False)
+
+    df = pd.read_csv(config.test)
+
+    do(config)
+    evaluate(df, config)
