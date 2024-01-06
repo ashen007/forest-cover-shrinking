71f19140812ab71e9faa3eb3b405669b795e37ad
ashen007 <asheniranga@gmail.com>
2024-01-06 16:09:22 +0530

    new modules for residual attention - results

--- forest_cover_change_detection/models/v2/experimental/ex_7/modules.py
+++ forest_cover_change_detection/models/v2/experimental/ex_7/modules.py
@@ -12,6 +12,7 @@ class StageZero(nn.Module):
         super(StageZero, self).__init__()
 
         pre_process_layers = []
+        post_process_layers = []
         trunk_layers = []
         inter_channels = in_channels
 
@@ -23,8 +24,6 @@ class StageZero(nn.Module):
             inter_channels = out_channels
 
         # trunk layers
-        inter_channels = in_channels
-
         for _ in range(t):
             trunk_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
             inter_channels = out_channels
@@ -43,6 +42,10 @@ class StageZero(nn.Module):
                        nn.Sigmoid()
                        ]
         mask_tail_layers += tail_layers
+
+        # post-process layers
+        for _ in range(p):
+            post_process_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
 
         # down
         down_1_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
@@ -83,7 +86,7 @@ class StageZero(nn.Module):
         self.trunk = nn.Sequential(*trunk_layers)
         self.mask_head = nn.Sequential(*mask_head_layers)
         self.mask_tail = nn.Sequential(*mask_tail_layers)
-        self.post_process = nn.Sequential(*pre_process_layers)
+        self.post_process = nn.Sequential(*post_process_layers)
 
     def forward(self, x):
         x = self.pre_process(x)
@@ -117,12 +120,13 @@ class StageZero(nn.Module):
 
 
 class StageOne(nn.Module):
-    """for level: 32 dimensions"""
+    """for level: 64 dimensions"""
 
     def __init__(self, in_channels, out_channels, p=1, r=1, t=2):
         super(StageOne, self).__init__()
 
         pre_process_layers = []
+        post_process_layers = []
         trunk_layers = []
         inter_channels = in_channels
 
@@ -134,8 +138,6 @@ class StageOne(nn.Module):
             inter_channels = out_channels
 
         # trunk layers
-        inter_channels = in_channels
-
         for _ in range(t):
             trunk_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
             inter_channels = out_channels
@@ -154,6 +156,120 @@ class StageOne(nn.Module):
                        nn.Sigmoid()
                        ]
         mask_tail_layers += tail_layers
+
+        # post-process layers
+        for _ in range(p):
+            post_process_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
+
+        # down
+        down_1_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.down_1 = nn.Sequential(nn.MaxPool2d(2),
+                                    *down_1_layers)
+
+        down_2_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.down_2 = nn.Sequential(nn.MaxPool2d(2),
+                                    *down_2_layers)
+
+        down_3_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.down_3 = nn.Sequential(nn.MaxPool2d(2),
+                                    *down_3_layers)
+
+        down_4_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.down_4 = nn.Sequential(nn.MaxPool2d(2),
+                                    *down_4_layers)
+
+        # up
+        up_4_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.up_4 = nn.Sequential(*up_4_layers,
+                                  nn.Upsample(scale_factor=2))
+
+        up_3_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.up_3 = nn.Sequential(*up_3_layers,
+                                  nn.Upsample(scale_factor=2))
+
+        up_2_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.up_2 = nn.Sequential(*up_2_layers,
+                                  nn.Upsample(scale_factor=2))
+
+        up_1_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
+        self.up_1 = nn.Sequential(*up_1_layers,
+                                  nn.Upsample(scale_factor=2))
+
+        # assembling
+        self.pre_process = nn.Sequential(*pre_process_layers)
+        self.trunk = nn.Sequential(*trunk_layers)
+        self.mask_head = nn.Sequential(*mask_head_layers)
+        self.mask_tail = nn.Sequential(*mask_tail_layers)
+        self.post_process = nn.Sequential(*post_process_layers)
+
+    def forward(self, x):
+        x = self.pre_process(x)
+        trunk_branch = self.trunk(x)
+
+        # mask branch
+        x_ = self.mask_head(x)
+        d1 = self.down_1(x_)
+        d2 = self.down_2(d1)
+        d3 = self.down_3(d2)
+
+        u3 = self.up_3(d3)
+
+        u3 += d2
+        u2 = self.up_2(u3)
+
+        u2 += d1
+        u1 = self.up_1(u2)
+
+        u1 += x_
+        out_ = self.mask_tail(u1)
+
+        # assembling
+        out_ = (trunk_branch * out_) + trunk_branch
+
+        return self.post_process(out_)
+
+
+class StageTwo(nn.Module):
+    """for level: 32 dimensions"""
+
+    def __init__(self, in_channels, out_channels, p=1, r=1, t=2):
+        super(StageTwo, self).__init__()
+
+        pre_process_layers = []
+        post_process_layers = []
+        trunk_layers = []
+        inter_channels = in_channels
+
+        self.mx_pool = nn.MaxPool2d(2)
+
+        # pre-process layers
+        for _ in range(p):
+            pre_process_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
+            inter_channels = out_channels
+
+        # trunk layers
+        for _ in range(t):
+            trunk_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
+            inter_channels = out_channels
+
+        # soft mask branch
+        mask_head_layers = [nn.MaxPool2d(2)]
+        mask_tail_layers = []
+
+        for _ in range(r):
+            mask_head_layers.append(ResNeStBlock(inter_channels, inter_channels, 2, 2, 40))
+            mask_tail_layers.append(ResNeStBlock(inter_channels, inter_channels, 2, 2, 40))
+
+        tail_layers = [nn.Upsample(scale_factor=2),
+                       nn.Conv2d(inter_channels, inter_channels, 1),
+                       nn.Conv2d(inter_channels, inter_channels, 1),
+                       nn.Sigmoid()
+                       ]
+        mask_tail_layers += tail_layers
+
+        # post-process layers
+        for _ in range(p):
+            post_process_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
 
         # down
         down_1_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
@@ -178,7 +294,7 @@ class StageOne(nn.Module):
         self.trunk = nn.Sequential(*trunk_layers)
         self.mask_head = nn.Sequential(*mask_head_layers)
         self.mask_tail = nn.Sequential(*mask_tail_layers)
-        self.post_process = nn.Sequential(*pre_process_layers)
+        self.post_process = nn.Sequential(*post_process_layers)
 
     def forward(self, x):
         x = self.pre_process(x)
@@ -203,13 +319,14 @@ class StageOne(nn.Module):
         return self.post_process(out_)
 
 
-class StageTwo(nn.Module):
+class StageThree(nn.Module):
     """for level: 32 dimensions"""
 
     def __init__(self, in_channels, out_channels, p=1, r=1, t=2):
-        super(StageTwo, self).__init__()
+        super(StageThree, self).__init__()
 
         pre_process_layers = []
+        post_process_layers = []
         trunk_layers = []
         inter_channels = in_channels
 
@@ -221,8 +338,6 @@ class StageTwo(nn.Module):
             inter_channels = out_channels
 
         # trunk layers
-        inter_channels = in_channels
-
         for _ in range(t):
             trunk_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
             inter_channels = out_channels
@@ -242,6 +357,10 @@ class StageTwo(nn.Module):
                        ]
         mask_tail_layers += tail_layers
 
+        # post-process layers
+        for _ in range(p):
+            post_process_layers.append(ResNeStBlock(inter_channels, out_channels, 2, 2, 40))
+
         # down
         down_1_layers = [ResNeStBlock(inter_channels, inter_channels, 2, 2, 40) for _ in range(r)]
         self.down_1 = nn.Sequential(nn.MaxPool2d(2),
@@ -256,7 +375,7 @@ class StageTwo(nn.Module):
         self.trunk = nn.Sequential(*trunk_layers)
         self.mask_head = nn.Sequential(*mask_head_layers)
         self.mask_tail = nn.Sequential(*mask_tail_layers)
-        self.post_process = nn.Sequential(*pre_process_layers)
+        self.post_process = nn.Sequential(*post_process_layers)
 
     def forward(self, x):
         x = self.pre_process(x)
@@ -265,8 +384,6 @@ class StageTwo(nn.Module):
         # mask branch
         x_ = self.mask_head(x)
         d1 = self.down_1(x_)
-
-        print(d1.shape)
 
         u1 = self.up_1(d1)
 
@@ -280,7 +397,7 @@ class StageTwo(nn.Module):
 
 
 if __name__ == '__main__':
-    t = torch.randn(4, 32, 16, 16)
-    m = StageTwo(32, 32, 2)
+    t = torch.randn(4, 128, 8, 8)
+    m = StageThree(128, 256)
 
     print(m(t).shape)
